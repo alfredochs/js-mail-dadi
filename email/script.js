533cf1571b6649987b883126d96b0368e15e5cb7
alfredochs <chanta.alfredo@gmail.com>
2021-10-13 17:06:45 +0200

    exercise done

--- email/script.js
+++ email/script.js
@@ -4,17 +4,17 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 
 const emailContainer = ["email1", "email2", "email3"];
-let utenteEsiste = true;
+let utenteEsiste = false;
 const richiestaEmail = prompt("Inserisci la tua email:");
 
 for (let i = 0; i < emailContainer.length; i++) {
     const emailCorrente = emailContainer[i];
     if (emailCorrente === richiestaEmail) {
         utenteEsiste = true;
-        alert("Utente Trovato");
     }
-    if (!emailCorrente === richiestaEmail) {
-        utenteEsiste = false;
-        alert("Utente Non trovato");
-    }
+}
+if (utenteEsiste) {
+    alert("Utente Trovato");
+} else {
+    alert("Utente non trovato");
 }
